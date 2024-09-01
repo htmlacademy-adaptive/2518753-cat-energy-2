f99874d60af32596bfe6343a607c6bcc1d20fa2c
Artem-Sobolev91 <167480489+Artem-Sobolev91@users.noreply.github.com>
2024-09-01 15:02:19 +0300

    добавляет скрипт на кнопку меню

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,15 @@
 /* в этот файл добавляет скрипты*/
+const navMain = document.querySelector('.main-nav');
+const navBurger = document.querySelector('.main-nav__burger');
+
+navMain.classList.remove('main-nav--nojs');
+
+navBurger.addEventListener('click', () => {
+  if (navMain.classList.contains('main-nav--closed')) {
+    navMain.classList.remove('main-nav--closed');
+    navMain.classList.add('main-nav--opened');
+  } else {
+    navMain.classList.add('main-nav--closed');
+    navMain.classList.remove('main-nav--opened');
+  }
+});
