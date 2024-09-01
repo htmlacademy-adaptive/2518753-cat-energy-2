4d3e98d3c1384a60a85db21b8074145067425a10
Artem-Sobolev91 <167480489+Artem-Sobolev91@users.noreply.github.com>
2024-09-01 15:37:33 +0300

    добавляет скрипт

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,5 +1,5 @@
 /* в этот файл добавляет скрипты*/
-const navMain = document.querySelector('.main-nav');
+const navMain = document.querySelector('.main-nav ');
 const navBurger = document.querySelector('.main-nav__burger');
 
 navMain.classList.remove('main-nav--nojs');
@@ -12,4 +12,23 @@ navBurger.addEventListener('click', () => {
     navMain.classList.add('main-nav--closed');
     navMain.classList.remove('main-nav--opened');
   }
+
+
 });
+
+
+const mainIcon = document.querySelector('.main-header__icon');
+
+
+navBurger.addEventListener('click', () => {
+  if (mainIcon.classList.contains('main-icon--opened')) {
+    mainIcon.classList.remove('main-icon--opened');
+    mainIcon.classList.add('main-icon--closed');
+  } else {
+    mainIcon.classList.add('main-icon--opened');
+    mainIcon.classList.remove('main-icon--closed');
+  }
+
+
+});
+
