82005de9014105694ed74d4545751a0d9c4c9703
Artem-Sobolev91 <167480489+Artem-Sobolev91@users.noreply.github.com>
2024-10-08 16:43:12 +0300

    решает работу карты без js

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,8 +1,10 @@
 /* в этот файл добавляет скрипты*/
 const navMain = document.querySelector('.main-nav ');
 const navBurger = document.querySelector('.main-header__burger');
+const mapPicture = document.querySelector('.map__picture');
 
 navMain.classList.remove('main-nav--nojs');
+mapPicture.classList.remove('map__picture--nojs');
 
 navBurger.addEventListener('click', () => {
   if (navMain.classList.contains('main-nav--closed')) {
